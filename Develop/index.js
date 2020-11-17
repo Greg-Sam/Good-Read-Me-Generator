d4ad7f3b749cef566a0536de52e47064e7d32f74
Greg Samuelson <grssam@gmail.com>
2020-11-17 14:25:53 -0800

    questions array

--- Develop/index.js
+++ Develop/index.js
@@ -1,16 +1,16 @@
+// const inquirer = require('inquirer')
 // array of questions for user
-const questions = [
+const questions = ['What is the name of your app?', 'What does the app do?', 'Do you want a table of contents?', 'What are the instalation instructions?', 'What is the usage information?', 'What are the contribution guidelines?', 'What are the instructions for testing?'
+]
+console.log(questions)
+// // function to write README file
+// function writeToFile(fileName, data) {
+// }
 
-];
+// // function to initialize program
+// function init() {
 
-// function to write README file
-function writeToFile(fileName, data) {
-}
+// }
 
-// function to initialize program
-function init() {
-
-}
-
-// function call to initialize program
-init();
+// // function call to initialize program
+// init();
