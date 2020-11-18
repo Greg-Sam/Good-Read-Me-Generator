08fcb1593b38dc02f18540dbf7c092181d0fa77f
Greg Samuelson <grssam@gmail.com>
2020-11-17 17:09:51 -0800

    built generateMarkdown

---
+++ test.md
@@ -0,0 +1,25 @@
+# myProject
+  It's pretty cool
+  ***
+  ## Table of Contents
+  Installation  
+  Usage  
+  License  
+  Contributing  
+  Tests  
+  Questions  
+  ## Instalation instructions
+  Click and hope for the best  
+  ## Usage description
+  not for children under 10 years old  
+  ## License information
+  public domain  
+  ## Usage information
+  not for children under 10 years old  
+  ## Contribution instructions
+  Bitcoin only  
+  ## Testing procedure
+  undefined  
+  ## Questions  
+  If you have any questions you can reach me at name@gmail.com 
+
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,27 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.name}
+  return `# ${data.title}
+  ${data.description}
+  ***
+  ## Table of Contents
+  Installation  
+  Usage  
+  License  
+  Contributing  
+  Tests  
+  Questions  
+  ## Instalation instructions
+  ${data.instalation}  
+  ## Usage description
+  ${data.usage}  
+  ## License information
+  ${data.license}   
+  ## Contribution instructions
+  ${data.contributing}  
+  ## Testing procedure
+  ${data.testing}  
+  ## Questions  
+  If you have any questions you can reach me at ${data.email} 
 
 `;
 }
