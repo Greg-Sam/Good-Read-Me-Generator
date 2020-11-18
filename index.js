879c0608398ff41501c38ce681d748cb6ed53bbe
Greg Samuelson <grssam@gmail.com>
2020-11-17 16:02:59 -0800

    after tutoring session

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
 const inquirer = require('inquirer')
+const fs = require('fs')
+let generateMarkdown = require('./utils/generateMarkdown')
 // array of questions for user
-const questions = ['What is the name of your app?', 'What does the app do?', 'Do you want a table of contents?', 'What are the instalation instructions?', 'What is the usage information?', 'What are the contribution guidelines?', 'What are the instructions for testing?'
+const questions = [{ type: 'input', name: 'name', message: 'What is the name of your app?' }, { type: 'input', name: 'description', message:'What does the app do?'}
 ]
 
 // // function to write README file
 function writeToFile(fileName, data) {
-  let markdown = require('./utils/generateMarkdown')
+  fs.writeFile(fileName, data, (err) => {
+    if (err) { console.log(err) }})
 }
 
 // // function to initialize program
 function init() {
-  inquirer.prompt([
-    {type: input,
-    name: name,
-      message: question[0]}
-  ])
+  inquirer.prompt(questions)
   .then(answers => {
     console.log(answers)
+    const test = generateMarkdown(answers)
+    console.log(test)
+    writeToFile('test.md', generateMarkdown(answers))
   })
-  .catch(err) => console.log(err)
+  .catch(err => console.log(err))
 
 }
 
